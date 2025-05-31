7d8a4d84c163a031eca14be0c99bec398dc74119
rahulecoglow <rahulsureshuk@gmail.com>
2025-05-31 11:26:34 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>EcoGlow Energy</title>
+      </Head>
+      <h1>Welcome to EcoGlow</h1>
+      <p>This is the homepage.</p>
+    </div>
+  )
+}
